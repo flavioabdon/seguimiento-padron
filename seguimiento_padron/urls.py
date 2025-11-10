812ec8212ed7fb279f37b09259d3a93829a394c5
flavio abdon <flaviocondori@gmail.com>
2025-11-10 14:06:08 -0400

    Remove Grappelli URL from urlpatterns

--- seguimiento_padron/urls.py
+++ seguimiento_padron/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('operaciones.urls')),
 ]
